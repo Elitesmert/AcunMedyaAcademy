3faf09b47828b580fb1e198d04ce71354aad0f8f
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-11 12:06:54 +0300

    Faydalı link güncelleme ve silme apileri düzenlendi

--- Backend/apps/useful_links/urls.py
+++ Backend/apps/useful_links/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ListUsefulLinksAPI, CreateUsefulLinksAPI
+from .views import (ListUsefulLinksAPI, CreateUsefulLinksAPI, UpdateUsefulLinkAPI, DeleteUsefulLinkAPI,)
 
 urlpatterns = [
     path('', ListUsefulLinksAPI.as_view(), name='useful_links_list'),
     path('add/', CreateUsefulLinksAPI.as_view(), name='useful_links_create'),
+    path('update/<pk>/', UpdateUsefulLinkAPI.as_view(), name='useful_links_update'),
+    path('delete/<pk>/', DeleteUsefulLinkAPI.as_view(), name='useful_links_delete'),
 ]
--- Backend/apps/useful_links/views.py
+++ Backend/apps/useful_links/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
 from .models import UsefulLinksModel
 from .serializers import UsefulLinksSerializer
 
@@ -15,3 +15,15 @@ class CreateUsefulLinksAPI(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class UpdateUsefulLinkAPI(RetrieveUpdateAPIView):
+    queryset = UsefulLinksModel.objects.all()
+    serializer_class = UsefulLinksSerializer
+    lookup_field = 'pk'
+
+
+class DeleteUsefulLinkAPI(DestroyAPIView):
+    queryset = UsefulLinksModel.objects.all()
+    serializer_class = UsefulLinksSerializer
+    lookup_field = 'pk'
