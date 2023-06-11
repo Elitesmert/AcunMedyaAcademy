339a6e632739196c5cc175c5bd4f595f9f4ee72b
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-11 14:31:40 +0300

    ALLOWED_HOST ayarında heroku domain hatası düzeltildi

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # HOST SETTINGS
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['django-api-czay.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['academy-project-api-furkan.herokuapp.com', '127.0.0.1']
 
 # RESTFRAMEWORK SETTINGS
 REST_FRAMEWORK = {
