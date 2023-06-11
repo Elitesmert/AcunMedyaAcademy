a4ef889caacb55cf2ef6f2150ef08eccaae0f192
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-11 14:54:02 +0300

    Heroku PostgreSQL ayarları yapılandırıldı

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 import environ
 
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -84,8 +85,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
