dd841d0e3c8e0d5e97f693ce160a5d0969d3b284
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-11 19:22:26 +0300

    CORS HEADERS ayarları yapılandırıldı

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -36,6 +36,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15)
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+]
+
 # INSTALLED APPS SETTINGS
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -49,6 +54,7 @@ INSTALLED_APPS = [
     # REST FRAMEWORK APPS
     'rest_framework_simplejwt',
     'rest_framework',
+    'corsheaders',
 ]
 # MIDDLEWARE SETTINGS
 MIDDLEWARE = [
@@ -59,6 +65,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # REST FRAMEWORK MIDDLEWARES
+    'corsheaders.middleware.CorsMiddleware',
 ]
 # ROOT URL SETTINGS
 ROOT_URLCONF = 'config.urls'
