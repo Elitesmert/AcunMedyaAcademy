746d7f788411af4f72143f26dfb060c93dbca181
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-12 16:31:25 +0300

    Video listeleme API view hazırlandı, serializer hazırlanacak...

--- Backend/apps/videos/views.py
+++ Backend/apps/videos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from .models import VideoModel, VideoCommentModel
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ListVideosAPI(ListAPIView):
+    queryset = VideoModel.objects.all()
+    permission_classes = [IsAuthenticated]
