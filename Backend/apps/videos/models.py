71a62fa2769f44d2d85e12f90be1ffb8c35c823a
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-12 16:28:45 +0300

    videos uygulamasının modelleri hazırlandı

--- Backend/apps/videos/models.py
+++ Backend/apps/videos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from autoslug import AutoSlugField
+from ..account.models import CustomUserModel, DepartmentModel
 
-# Create your models here.
+
+class VideoModel(models.Model):
+    title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title', unique=True)
+    description = models.TextField()
+    video = models.FileField(upload_to='videos/')
+    instructor = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE, related_name='videos',
+                                   limit_choices_to={"groups": 2})
+    department = models.ForeignKey(DepartmentModel, on_delete=models.CASCADE, related_name='videos', null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'videos'
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videolar'
+
+    def __str__(self):
+        return self.title
+
+
+class VideoCommentModel(models.Model):
+    author = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE, related_name='comments')
+    video = models.ForeignKey(VideoModel, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'video_comments'
+        verbose_name = 'Video Yorumu'
+        verbose_name_plural = 'Video Yorumları'
+
+    def __str__(self):
+        return self.author.get_full_name()
