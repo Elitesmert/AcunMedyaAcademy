57af56cfb4202022e1a050c02eb02fb09dcc163c
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-14 18:06:21 +0300

    Uygulama isimlerine verbose name uygulandı

--- Backend/apps/useful_links/apps.py
+++ Backend/apps/useful_links/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UsefulLinksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.useful_links'
+    verbose_name = 'Faydalı Link Yönetimi'
--- Backend/apps/videos/apps.py
+++ Backend/apps/videos/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class VideosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.videos'
+    verbose_name = 'Video Yönetimi'
 
     def ready(self):
         from .signals import send_discord_message
