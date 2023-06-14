0415e2c1d083471a4baae205fe16635485ae4239
Furkan Özay <furkann.ozay@hotmail.com>
2023-06-14 11:51:31 +0300

    useful_links serializerda düzenleme yapıldı

--- Backend/apps/useful_links/serializers.py
+++ Backend/apps/useful_links/serializers.py
@@ -1,17 +1,11 @@
 from rest_framework import serializers
 from .models import UsefulLinksModel
+from ..account.serializers import UserSerializer
 
 
 class UsefulLinksSerializer(serializers.ModelSerializer):
-    username = serializers.SerializerMethodField(method_name='get_username')
-    user_avatar = serializers.SerializerMethodField(method_name='get_user_avatar')
+    author = UserSerializer()
 
     class Meta:
         model = UsefulLinksModel
-        fields = ['name', 'link', 'username', 'user_avatar', 'language', 'created_on', 'updated_on']
-
-    def get_username(self, obj):
-        return str(obj.author.username)
-
-    def get_user_avatar(self, obj):
-        return str(obj.author.avatar.url)
+        fields = '__all__'
--- Backend/apps/useful_links/urls.py
+++ Backend/apps/useful_links/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (ListUsefulLinksAPI, CreateUsefulLinksAPI, UpdateUsefulLinkAPI, DeleteUsefulLinkAPI,)
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', ListUsefulLinksAPI.as_view(), name='useful_links_list'),
+    path('', cache_page(60 * 1)(ListUsefulLinksAPI.as_view()), name='useful_links_list'),
     path('add/', CreateUsefulLinksAPI.as_view(), name='useful_links_create'),
     path('update/<pk>/', UpdateUsefulLinkAPI.as_view(), name='useful_links_update'),
     path('delete/<pk>/', DeleteUsefulLinkAPI.as_view(), name='useful_links_delete'),
